3843ee6bb6bb1b64cc6a5e0ba9325e945fa35a17
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-19 11:41:56 -0300

    feat(frontend): configurate socket.io

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,4 +1,11 @@
+import { io } from 'socket.io-client'
+const socket = io('http://localhost:3333');
+
 function App() {
+  socket.on('connect', () => {
+    console.log(socket)
+  })
+
   return (
     <>
       Hello World
