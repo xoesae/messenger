ad80affadf6582b95406510a29b6415a6e533e5a
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-19 20:59:49 -0300

    feat(frontend): add components of material ui
    n

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import { io } from 'socket.io-client'
+import React, { useState, useRef } from 'react';
+import { Input, Button } from '@material-ui/core'
 
+import { io } from 'socket.io-client'
 const socket = io('http://localhost:3333')
 
 function App() {
   const [messages, setMessages] = useState([])
   const [message, setMessage] = useState('')
+  let input = useRef(null)
 
   socket.on('connect', () => {
     socket.on('messages', (arg) => {
@@ -13,24 +15,26 @@ function App() {
     })
   })
 
-  function handleChange(event) {
-    setMessage(event.target.value)
+  function handleChange() {
+    setMessage(input.current.value)
   }
 
   function handleClick(){
-    socket.emit('message', message);
-    console.log(message)
+    socket.emit('message', message)
+    input.current.value = ''
+    handleChange()
   }
 
   return (
     <div>
       <p>{messages}</p>
-      <input
+      <Input
         type="text"
-        className="input"
+        placeholder="Type a message"
+        inputRef={input}
         onChange={handleChange}
       />
-      <button onClick={handleClick}>Enviar</button>
+      <Button onClick={handleClick}>Enviar</Button>
     </div>
   );
 }
