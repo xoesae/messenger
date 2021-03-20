765c6c64d604dc5c15126ba95a0373f9272d490e
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-20 13:16:37 -0300

    refactor(frontend): organizate structure of folders

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,44 +1,8 @@
-import React, { useState, useRef } from 'react';
-import { Input, Button } from '@material-ui/core'
-
-import { io } from 'socket.io-client'
-const socket = io('http://localhost:3333')
+import Main from './views/Main';
 
 function App() {
-  const [messages, setMessages] = useState([])
-  const [message, setMessage] = useState('')
-  let input = useRef(null)
-
-  socket.on('connect', () => {
-    socket.on('messages', (arg) => {
-      setMessages(arg)
-    })
-  })
-
-  function handleChange() {
-    setMessage(input.current.value)
-  }
-
-  function handleClick(){
-    socket.emit('message', message)
-    input.current.value = ''
-    handleChange()
-  }
-
   return (
-    <div>
-      {messages.map((msg, i) => {
-           console.log(msg);
-           return (<p>{msg}</p>)
-      })}
-      <Input
-        type="text"
-        placeholder="Type a message"
-        inputRef={input}
-        onChange={handleChange}
-      />
-      <Button onClick={handleClick}>Enviar</Button>
-    </div>
+    <Main />
   );
 }
 
