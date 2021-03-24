5fc280ec7a33d6da2f7fbe3bc84beaa7a6398b85
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-24 16:25:55 -0300

    feat(backend): add a find all messages controller

--- packages/backend/src/controllers/message.controller.js
+++ packages/backend/src/controllers/message.controller.js
@@ -2,6 +2,15 @@ import Message from '../models/message.model'
 
 const messageController = {}
 
+messageController.allMessages = async function () {
+  try{
+    const messages = await Message.find({})
+    return messages
+  } catch(err){
+    console.log(err)
+  }
+}
+
 messageController.newMessage = async function () {
   let message = new Message({ author: 'sidnisadnasdld', text: 'my text' })
   message = await message.save()
