f93f31f2642015461b28e94cfd0c18035d881242
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-24 14:45:32 -0300

    feat(backend): add db config

---
+++ packages/backend/src/config/db.config.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const db = mongoose.connection
+
+mongoose.connect(
+  'mongodb://localhost/messenger',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {})
+
+export default db
