48984ae68de92cbc475864f7b7b60d9df65c5b3a
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-26 17:01:47 -0300

    refactor(frontend): remove variables not used

--- packages/frontend/src/views/Main/index.js
+++ packages/frontend/src/views/Main/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { Container, Chat, SendMessage, InputContainer, ButtonContainer, Message, Input, Button } from './style';
+import { Container, Chat, SendMessage, Message, Input, Button } from './style';
 
 import { io } from 'socket.io-client'
 const socket = io('http://localhost:3333')
