3a455f0d86086dd84d627b7b9aedc9f5cd6c4e32
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-26 16:51:42 -0300

    fix(frontend): add box-sizing

--- packages/frontend/src/views/Main/style.js
+++ packages/frontend/src/views/Main/style.js
@@ -16,6 +16,8 @@ export const Chat = styled.div`
 export const SendMessage = styled.div`
   width: 100%;
 
+  box-sizing: border-box;
+
   display: flex;
   flex-direction: row;
   align-items: center;
