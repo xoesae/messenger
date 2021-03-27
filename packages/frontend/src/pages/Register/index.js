6b07ac0982a7b9f299794e1c32be11946986db2b
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-27 11:04:58 -0300

    feat(frontend): add a handles functions

--- packages/frontend/src/pages/Register/index.js
+++ packages/frontend/src/pages/Register/index.js
@@ -1,6 +1,26 @@
+import React, { useState, useRef } from 'react'
 import { Container, Modal, Title, Input, Button } from './style'
 
 function Register() {
+  const [username, setUsername] = useState()
+  let input = useRef(null)
+
+  function handleKeyUp() {
+    setUsername(input.current.value)
+  }
+
+  function handleClick(){
+    if(!username.length === 0 || username.trim()){
+      
+    }
+  }
+
+  function handleKeyPress(event){
+    if(event.key === 'Enter'){
+      handleClick()
+    }
+  }
+
   return (
     <Container>
       <Modal>
@@ -10,8 +30,15 @@ function Register() {
         <Input
           type="text"
           placeholder="Type your username"
+          ref={input}
+          onKeyUp={handleKeyUp}
+          onKeyPress={handleKeyPress}
         />
-        <Button>Enter</Button>
+        <Button
+          onClick={handleClick}
+        >
+          Enter
+        </Button>
       </Modal>
     </Container>
   );
