f44a79e11e6b84cf5c9b58974c2cbdbd82f668a5
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-27 10:10:05 -0300

    refactor(backend): change de get messages to use async/await

--- packages/backend/src/utils/getMessages.js
+++ packages/backend/src/utils/getMessages.js
@@ -1,12 +1,17 @@
 import messageController from '../controllers/message.controller'
 
-const getMessages = messageController.allMessages().then((res) => {
-  if(res.sucess){
-    return res.messages
+async function getMessages(){
+  try{
+    const res = await messageController.allMessages()
+
+    if(res.sucess){
+      return res.messages
+    } else{
+      console.log(res)
+    }
+  } catch(err){
+    console.log(err)
   }
-})
-.catch((err) => {
-  console.log(err)
-})
+}
 
 export default getMessages
