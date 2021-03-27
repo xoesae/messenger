b8d66279f171c8e96ed89afaf43fbf2c122f8d01
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-27 10:53:30 -0300

    feat(frontend): create routes and paths

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,10 +1,26 @@
-import Main from './pages/Main';
-import './css/reset.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
+import Main from './pages/Main'
+import Register from './pages/Register'
+
+import './css/reset.css'
 import './css/global.css'
 
 function App() {
   return (
-    <Main />
+    <Router>
+      <Switch>
+         <Route path="/" exact={true}>
+           <Main />
+         </Route>
+         <Route path="/register">
+           <Register />
+         </Route>
+       </Switch>
+     </Router>
   );
 }
 
